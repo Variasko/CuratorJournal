05e34ead0c406b1813b7320bb8a7a193de4ea7af
Variasko <121598389+Variasko@users.noreply.github.com>
2025-03-26 20:38:40 +0300

    Загеристрированны существующие вью модели

--- CuratorJournal/CuratorJournal.Desktop/App.xaml.cs
+++ CuratorJournal/CuratorJournal.Desktop/App.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using CuratorJournal.Desktop.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CuratorJournal.Desktop
 {
@@ -13,5 +10,20 @@ namespace CuratorJournal.Desktop
     /// </summary>
     public partial class App : Application
     {
+        private static IServiceProvider _Services;
+
+        public static IServiceProvider Services => _Services ?? InitializeServices().BuildServiceProvider();
+
+        public static IServiceCollection InitializeServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<SignInWindowViewModel>();
+            services.AddSingleton<MainWindowViewModel>();
+
+            services.AddTransient<ProfilePageViewModel>();
+
+            return services;
+        }
     }
 }
