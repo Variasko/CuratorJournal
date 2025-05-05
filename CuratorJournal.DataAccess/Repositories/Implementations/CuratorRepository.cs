c9324c9e233f8ed4f10dcd08ed06cc40006010e2
Variasko <121598389+Variasko@users.noreply.github.com>
2025-05-05 20:23:24 +0300

    реализация репозитория для куратора

---
+++ CuratorJournal.DataAccess/Repositories/Implementations/CuratorRepository.cs
@@ -0,0 +1,13 @@
+﻿using CuratorJournal.DataAccess.Database;
+using CuratorJournal.DataAccess.Models;
+using CuratorJournal.DataAccess.Repositories.Implementations.Base;
+using CuratorJournal.DataAccess.Repositories.Interfaces;
+
+namespace CuratorJournal.DataAccess.Repositories.Implementations
+{
+    public class CuratorRepository : BaseRepository<Curator>, ICuratorRepository
+    {
+        public CuratorRepository(ApplicationContext context)
+        : base(context) { }
+    }
+}
